1898bd2b77d629f249b829219bf8c8d4f90981d7
任大大大 <1732166552@qq.com>
2021-10-21 20:42:43 +0800

    更新1

--- HelloGit/src/main/java/com/renxiaobo/Git/HelloGit.java
+++ HelloGit/src/main/java/com/renxiaobo/Git/HelloGit.java
@@ -3,5 +3,6 @@ package com.renxiaobo.Git;
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("HelloGit");
+        System.out.println("更新1");
     }
 }
